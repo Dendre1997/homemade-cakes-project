e5f1316dd31a9c40060010bb2ce7eb80d5439620
Dendre1997@ <denyskichuk97@gmail.com>
2025-07-10 23:53:16 -0600

    feat(types): define data structure for Product entity

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,45 @@
-import { ObjectId } from "mongodb";
+import { ObjectId } from 'mongodb';
 
 export interface Flavor {
-    _id: ObjectId;
-    name: string;
-    price: number;
-    description?: string;
+  _id: ObjectId;
+  name: string;
+  price: number;
+  description?: string;
 }
 
 export interface Decoration {
-    _id: ObjectId;
-    name: string;
-    price: number;
-    imageUrl?: string; 
+  _id: ObjectId;
+  name: string;
+  price: number;
+  imageUrl?: string;
 }
 
 export interface Allergen {
-    _id: ObjectId;
-    name: string;
+  _id: ObjectId;
+  name: string;
 }
 
 export interface Diameter {
-    _id: ObjectId;
-    sizeValue: number;
-    unit: string;
+  _id: ObjectId;
+  sizeValue: number;
+  unit: string;
+}
+
+export interface AvailableDiameterConfig {
+  diameterId: ObjectId;
+  multiplier: number;
+}
+
+export interface Product {
+  _id: ObjectId;
+  name: string;
+  description: string;
+  imageUrls: string[];
+
+  structureBasePrice: number;
+  availableFlavorIds: ObjectId[];
+  availableDiameterConfigs: AvailableDiameterConfig[];
+  allergenIds: ObjectId[];
+
+  isActive: boolean;
 }
